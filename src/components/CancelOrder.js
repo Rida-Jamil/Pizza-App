039753a46d0d1411d1b432a8e467af5800511e9a
Rida Jamil <rida.jamil7586@gmail.com>
2021-09-10 11:07:25 +0500

    Place/Cancel Components

--- src/components/CancelOrder.js
+++ src/components/CancelOrder.js
@@ -1,16 +1,78 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { Button, Table } from 'react-bootstrap';
 
+const tablestyle = {
+    color: "orange",
+};
 
 function CancelOrder() {
     const history = useHistory();
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        const requestOptions = {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(`https://order-pizza-api.herokuapp.com/api/orders`, requestOptions)
+            .then(response => response.json())
+            .then(data => setOrders(data))
+    },
+        []);
+
+    const onClickCancel = (Order_ID) => {
+        const requestOptions = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        };
+
+        fetch(`https://order-pizza-api.herokuapp.com/api/orders/${Order_ID}`, requestOptions)
+            .then(response => {
+                if (response.status === 200) {
+                    alert('Cancelled successfully')
+                }
+                window.location.reload();
+            })
+    }
+
     return (
         <>
             <h3>Cancel Orders List</h3>
+            <Table striped bordered hover variant="warning" style={tablestyle}>
+                <thead>
+                    <tr>
+                        <th>Order ID#</th>
+                        <th>Crust</th>
+                        <th>Flavour</th>
+                        <th>Size</th>
+                        <th>Time Stamp</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {orders.map((v, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{v.Order_ID}</td>
+                                <td>{v.Crust}</td>
+                                <td>{v.Flavor}</td>
+                                <td>{v.Size}</td>
+                                <td>{v.Timestamp}</td>
+                                <td><Button variant="outline-warning" size="md" onClick={() => onClickCancel(v.Order_ID)}>Cancel Order</Button></td>
+                            </tr>
+                        )
+                    }
+                    )
+                    }
+                </tbody>
+            </Table>
+
             <Button variant="outline-warning" size="lg" onClick={() => history.goBack()}> Go Back </Button>
+
         </>
     )
 }
 
 export default CancelOrder;
+
--- src/components/PlaceOrder.js
+++ src/components/PlaceOrder.js
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
+import { Button } from 'react-bootstrap';
 
 
 function PlaceOrder() {
     const history = useHistory();
+    const [crust, setCrust] = useState('');
+    const [flavor, setFlavor] = useState('');
+    const [size, setSize] = useState('');
+
+    const onChangeCrust = (event) => {
+        setCrust(event.target.value);
+    }
+
+    const onChangeFlavor = (event) => {
+        setFlavor(event.target.value);
+    }
+
+    const onChangeSize = (event) => {
+        setSize(event.target.value);
+    }
+
+    const handleClick = () => {
+        console.log(crust, flavor, size);
+        if (crust === "" || flavor === "" || size === "") {
+            alert('please select appropriate options')
+        }
+        else {
+            let pizzaObject = {
+                "Flavor": flavor,
+                "Crust": crust,
+                "Size": size,
+                "Table_No": 9,
+            }
+
+            let token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzExNzE5MTksIm5iZiI6MTYzMTE3MTkxOSwianRpIjoiMmRjM2MwZWYtYWZlNy00Y2QwLWIyNTMtM2RlMWM4MTRlYzRiIiwiZXhwIjoxNjMxMTcyODE5LCJpZGVudGl0eSI6InRlc3QiLCJmcmVzaCI6ZmFsc2UsInR5cGUiOiJhY2Nlc3MifQ.S6t5pUBgCjIwJ3FJr8ifG3itxmb06Rux7aG4Yf9ZE0U";
+
+            const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+                body: JSON.stringify(pizzaObject)
+            };
+
+            fetch(`https://order-pizza-api.herokuapp.com/api/orders`, requestOptions)
+                .then(response => {
+                    if (response.status === 201) {
+                        alert('Your order has been registered')
+                    }
+                })
+        }
+    }
+
+
     return (
         <>
             <h3>What do you Like? </h3>
-            <Button variant="outline-warning" size="lg" onClick={() => history.push('./ViewOrder')}> View Order
-            </Button>
+            
+            <label for="cars">Crust:</label>
+            <select name="crust" onChange={onChangeCrust}>
+                <option value="Select" selected> Select </option>
+                <option value="STUFFED">Stuffed</option>
+                <option value="CRACKER">Cracker</option>
+                <option value="THIN">Thin</option>
+            </select>
+            <label for="flavor">Flavor:</label>
+            <select name="flavor" onChange={onChangeFlavor}>
+                <option value="Select" selected> Select </option>
+                <option value="FAJITA">Fajita</option>
+                <option value="CHEESE">Cheese</option>
+                <option value="VEGGIE">Veggie</option>
+            </select>
+            <label for="size">Size:</label>
+            <select name="size" onChange={onChangeSize}>
+                <option value="Select" selected> Select </option>
+                <option value="S">Small</option>
+                <option value="M">Medium</option>
+                <option value="L">Large</option>
+            </select>
+
+            <Button variant="outline-warning" size="lg" onClick={handleClick}>Click here</Button>
             <Button variant="outline-warning" size="lg" onClick={() => history.goBack()}> Go Back </Button>
         </>
     )
